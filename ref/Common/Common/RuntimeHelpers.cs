7b2802dfa0b2ea7582a6a534e28386f443ed8a3c
AraHaan <seandhunt_7@yahoo.com>
2022-08-22 11:25:06 -0400

    Fixed plugin loader to prefer loading RID specific plugins after non-rid specific ones are loaded.
    This will fix the problem when trying to load plugins made in C++ /CLR.
    
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

---
+++ ref/Common/Common/RuntimeHelpers.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2018-2022, Els_kom org.
+// https://github.com/Elskom/
+// All rights reserved.
+// license: MIT, see LICENSE for more details.
+
+namespace Elskom.Generic.Libs;
+
+/// <summary>
+/// Internal Runtime Helpers.
+/// </summary>
+public static class RuntimeHelpers
+{
+    /// <summary>
+    /// Gets the runtime identifier for the current process,
+    /// that is it transforms the operating system and the process architecture
+    /// of the current process into an valid runtime identifier that the .NET SDK has.
+    /// </summary>
+    /// <returns>
+    /// An Runtime Identifier string, or null if it cannot be converted into an runtime identifier.
+    /// </returns>
+    public static string GetCurrentRuntimeIdentifier()
+        => throw null;
+}
